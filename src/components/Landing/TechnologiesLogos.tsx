ded6c62ee50a0e42a274ce9cb15a88ca1e0e23c2
shaadcode <shaadcode@gmail.com>
2026-09-21 17:57:56 +0330

    fix: Failure to load technology images

--- src/components/Landing/TechnologiesLogos.tsx
+++ src/components/Landing/TechnologiesLogos.tsx
@@ -4,35 +4,35 @@ import { LogoStepper } from '../ui/logo-stepper'
 
 const logos: ComponentProps<typeof LogoStepper>['logos'] = [
   {
-    icon: <Image src="/logos/bun.svg" alt="Bun" width={40} height={40} />,
+    icon: <Image src="/shaadkit-docs/logos/bun.svg" alt="Bun" width={40} height={40} />,
     label: 'Bun',
   },
   {
-    icon: <Image src="/logos/eslint.svg" alt="ESLint" width={40} height={40} />,
+    icon: <Image src="/shaadkit-docs/logos/eslint.svg" alt="ESLint" width={40} height={40} />,
     label: 'ESLint',
   },
   {
-    icon: <Image src="/logos/mantine.svg" alt="Mantine" width={40} height={40} />,
+    icon: <Image src="/shaadkit-docs/logos/mantine.svg" alt="Mantine" width={40} height={40} />,
     label: 'Mantine',
   },
   {
-    icon: <Image src="/logos/nextjs.svg" alt="Next.js" width={40} height={40} />,
+    icon: <Image src="/shaadkit-docs/logos/nextjs.svg" alt="Next.js" width={40} height={40} />,
     label: 'Next.js',
   },
   {
-    icon: <Image src="/logos/postcss.svg" alt="PostCSS" width={40} height={40} />,
+    icon: <Image src="/shaadkit-docs/logos/postcss.svg" alt="PostCSS" width={40} height={40} />,
     label: 'PostCSS',
   },
   {
-    icon: <Image src="/logos/storybook.svg" alt="Storybook" width={40} height={40} />,
+    icon: <Image src="/shaadkit-docs/logos/storybook.svg" alt="Storybook" width={40} height={40} />,
     label: 'Storybook',
   },
   {
-    icon: <Image src="/logos/stylelint.svg" alt="Stylelint" width={40} height={40} />,
+    icon: <Image src="/shaadkit-docs/logos/stylelint.svg" alt="Stylelint" width={40} height={40} />,
     label: 'Stylelint',
   },
   {
-    icon: <Image src="/logos/typescript.svg" alt="TypeScript" width={40} height={40} />,
+    icon: <Image src="/shaadkit-docs/logos/typescript.svg" alt="TypeScript" width={40} height={40} />,
     label: 'TypeScript',
   },
 ]
