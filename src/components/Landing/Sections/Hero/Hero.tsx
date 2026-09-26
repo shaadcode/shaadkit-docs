e9d3f7a5c94ce16646bdb78f09f2aadb5ad0d6a2
shaadcode <shaadcode@gmail.com>
2026-09-26 17:31:46 +0330

    feat: add arktype and tanstack query in hero section

---
+++ src/components/Landing/Sections/Hero/Hero.tsx
@@ -0,0 +1,50 @@
+import { Button, Center, Grid, GridCol, Group, Stack, Text } from '@mantine/core';
+import AnimatedTexts from '../../AnimatedTexts';
+import TechnologiesLogos from '../../TechnologiesLogos';
+import classes from './Hero.module.css';
+
+function Hero() {
+  return (
+    <Grid
+      styles={{ inner: { height: '100%' } }}
+      h="90dvh"
+    >
+      <GridCol order={{ base: 2, lg: 1 }} span={{ base: 12, lg: 6 }}>
+        <Stack className={classes['detailsContainer']}>
+          <AnimatedTexts data={[
+            { word: 'Modern', props: { c: 'green' } },
+            { word: 'Scalable', props: { c: 'orange' } },
+            { word: 'Powerful', props: { c: 'yellow' } },
+            { word: 'Ready', props: { c: 'blue' } },
+          ]}
+          />
+          <Text>
+            <Text span fz={45} fw={700} tt="uppercase">shaadkit,</Text>
+            <br />
+            <Text span fz={{ base: 25, lg: 35 }} tt="uppercase">my production-oriented stack for Nextjs projects</Text>
+          </Text>
+          <Group mt="xl">
+            <Button
+              component="a"
+              target="_blank"
+              href="https://github.com/shaadcode/shaadkit"
+              color="dark"
+              radius={999}
+              tt="capitalize"
+              variant="outline"
+            >
+              repository
+            </Button>
+          </Group>
+        </Stack>
+      </GridCol>
+      <GridCol order={{ base: 1, lg: 2 }} span={{ base: 12, lg: 6 }}>
+        <Center h="100%">
+          <TechnologiesLogos />
+        </Center>
+      </GridCol>
+    </Grid>
+  )
+}
+
+export default Hero
